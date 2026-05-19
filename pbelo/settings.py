0ca2a79b29b88539b478918515b618369db477d6
Cole Gartner <cgartner@dakotafence.com>
2026-05-19 15:58:53 -0500

    WSGI setting changes

--- pbelo/settings.py
+++ pbelo/settings.py
@@ -10,22 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6g%zf3+t@fmpo5l@8yjs38&t)$&k+5l3&iv%4isfy0&p5uw%ik'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-6g%zf3+t@fmpo5l@8yjs38&t)$&k+5l3&iv%4isfy0&p5uw%ik')
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
